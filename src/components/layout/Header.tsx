d6fcf4c29f89856c8b456700662bf707f5290f9d
Andersen Cypriano <andersen.cypriano@gmail.com>
2025-10-27 11:36:14 -0300

    feat: style scrolled header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,25 +8,27 @@ import Logo from "../common/Logo";
 export default function Header() {
   const isMobile = useIsMobile();
   const isScrolled = useIsScrolled();
-  
+
   return (
     <div className="w-full fixed top-0 left-0 right-0 z-50 md:h-[auto]">
-        <div className={`relative h-20 mx-auto p-4 z-10 md:flex md:items-center md:justify-between md:rounded-lg transition-all duration-300 ${isScrolled ? 'md:bg-neutral-800' : 'md:bg-neutral-0'}`}>
-            {/* Mobile menu - only show on mobile after hydration */}
-            <div className="md:hidden absolute top-1/2 -translate-y-1/2">
-              {isMobile && <HamburgerMenu />}
-            </div>
-            
-            {/* Logo - centered on mobile, left on desktop */}
-            <div className="absolute left-1/2 -translate-x-1/2 top-1/2  -translate-y-1/2 md:relative md:left-auto md:translate-x-0">
-              <Logo width={80} height={91} />
-            </div>
-            
-            {/* Desktop menu - only show on desktop after hydration */}
-            <div className="hidden md:block">
-              {!isMobile && <Menu />}
-            </div>
+      <div className={`relative h-20 md:h-[auto] p-4 z-10 transition-all duration-300 ${isScrolled ? 'md:bg-gray-900' : 'md:bg-gray-0'} `}>
+        <div className="container relative mx-auto md:flex md:items-center md:justify-between">
+          {/* Mobile menu - only show on mobile after hydration */}
+          <div className="md:hidden absolute top-1/2 -translate-y-1/2">
+            {isMobile && <HamburgerMenu />}
+          </div>
+
+          {/* Logo - centered on mobile, left on desktop */}
+          <div className="absolute left-1/2 -translate-x-1/2 top-1/2  -translate-y-1/2 md:relative md:left-auto md:translate-x-0 md:top-[auto] md:translate-y-[auto]">
+            <Logo width={80} height={91} />
+          </div>
+
+          {/* Desktop menu - only show on desktop after hydration */}
+          <div className="hidden md:block">
+            {!isMobile && <Menu />}
+          </div>
         </div>
+      </div>
     </div>
   );
 }
