9273ad407989df7152ad53d1a919f5cf8775ea3c
Andersen Cypriano <andersen.cypriano@gmail.com>
2025-10-30 13:47:08 -0300

    feat: add props direction on menu component

--- src/components/layout/Menu.tsx
+++ src/components/layout/Menu.tsx
@@ -1,14 +1,24 @@
+import { IMenu } from "@/interfaces/IMenu";
 import { useIsScrolled } from "@/utils/IsScrolled";
 import Link from "next/link";
+import { use } from "react";
+import useDrawer from "../../../store/UseDrawerStore";
 
-export default function Menu() {
-  const isScrolled = useIsScrolled();
+
+
+export default function Menu({direction = 'row'}: IMenu) {
+
+  const menuStats = {
+    column: 'flex-col',
+    row: 'flex-row'
+  }
+
   return (
-    <div className="flex items-center gap-4">
+    <div className={`flex ${menuStats[direction]} gap-4 `}>
       <Link href="/" className="text-white">Home</Link>
-      <Link href="/sobre-nos" className="text-white">Sobre Nós</Link>
-      <Link href="/solucoes" className="text-white">Soluções</Link>
-      <Link href="/contato" className="text-white">Contato</Link>
+      <Link href="/" className="text-white colum">Sobre Nós</Link>
+      <Link href="/" className="text-white">Soluções</Link>
+      <Link href="/" className="text-white">Contato</Link>
     </div>
   );
 }
--- src/components/ui/Drawer.tsx
+++ src/components/ui/Drawer.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { X } from "lucide-react";
 import useDrawer from "../../../store/UseDrawerStore";
+import Menu from "../layout/Menu";
 
 export default function Drawer() {
   const {isOpen, onOpen} = useDrawer();
@@ -12,12 +13,13 @@ export default function Drawer() {
 
   return (
     <>
-      <div className={`fixed w-4/5 h-full p-4 transition-all duration-300 bg-gray-900 bg-opacity-80 z-50 ${isOpen ? drawer.open : drawer.close}` } id="overlay">
-        <div className="w-full flex justify-end">
+      <div className={`fixed w-full h-full p-4 transition-all duration-300  bg-gray-900/95 z-40 pt-32 ${isOpen ? drawer.open : drawer.close}` } id="overlay">
+        <div className="flex justify-end">
           <button className="cursor-pointer" onClick={onOpen}>
             <X color="white"/>
           </button>
         </div>
+        <Menu direction="column"/>
       </div>
     </>
   )
